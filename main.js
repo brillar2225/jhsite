e060ea1ac465cbd7d39b8563a2b60c503977ee95
Jaehee <brillar2225@gmail.com>
2022-11-09 18:48:04 +0700

    ADD: Add arrow up button

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 
 const header = document.querySelector('#header');
 const homeContainer = document.querySelector('#homeContainer');
+const about = document.querySelector('#about');
+const arrowUpBtn = document.querySelector('#arrowUpBtn');
 
-const fixHeader = () => {
+const handleScroll = () => {
   const scrollY = window.scrollY;
+
+  let opacity;
+
+  // Header Scroll Event
   const headerHeight = header.getBoundingClientRect().height;
 
   if (scrollY > headerHeight) {
@@ -12,16 +18,25 @@ const fixHeader = () => {
   } else {
     header.classList.remove('fixed');
   }
-};
 
-const scrollOpacity = () => {
-  const scrollY = window.scrollY;
+  // Home Scroll Event
   const homeContainerHeight = homeContainer.getBoundingClientRect().height;
-  const opacity = 1 - scrollY / homeContainerHeight;
-  // = (homeContainerHeight - scrollY) / homeContainerHeight
+  opacity = 1 - scrollY / homeContainerHeight; // = (homeContainerHeight - scrollY) / homeContainerHeight
 
   homeContainer.style.opacity = opacity;
+
+  // Arrow Up Event
+  const homeContainerBottom = homeContainer.getBoundingClientRect().bottom;
+  const aboutHeight = about.getBoundingClientRect().height;
+  opacity = scrollY / aboutHeight;
+
+  arrowUpBtn.style.opacity = opacity;
+
+  if (scrollY > homeContainerHeight / 2) {
+    arrowUpBtn.classList.add('visible');
+  } else {
+    arrowUpBtn.classList.remove('visible');
+  }
 };
 
-document.addEventListener('scroll', fixHeader);
-document.addEventListener('scroll', scrollOpacity);
+document.addEventListener('scroll', handleScroll);
