e40dd4d32e08cb73e2958c8b77dd2538db5c6a1a
Jaehee <brillar2225@gmail.com>
2022-11-09 16:46:44 +0700

    ADD: Add home transparent

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const header = document.querySelector('#header');
+const homeContainer = document.querySelector('#homeContainer');
 
 const fixHeader = () => {
   const scrollY = window.scrollY;
@@ -13,4 +14,14 @@ const fixHeader = () => {
   }
 };
 
+const scrollOpacity = () => {
+  const scrollY = window.scrollY;
+  const homeContainerHeight = homeContainer.getBoundingClientRect().height;
+  const opacity = 1 - scrollY / homeContainerHeight;
+  // = (homeContainerHeight - scrollY) / homeContainerHeight
+
+  homeContainer.style.opacity = opacity;
+};
+
 document.addEventListener('scroll', fixHeader);
+document.addEventListener('scroll', scrollOpacity);
