c0b99789b75b35aa706707cd391570891f77f90e
Jaehee <brillar2225@gmail.com>
2022-11-27 22:39:58 +0700

    FIX: Optimize the active button in the navigation bar when scrolling.

--- main.js
+++ main.js
@@ -4,20 +4,49 @@
 const header = document.querySelector('#header');
 const navbar = document.querySelector('#navbar');
 const menubarBtn = document.querySelector('#menubarBtn');
-const lists = document.querySelectorAll('.header__li');
-const sections = document.querySelectorAll('section');
+const listAll = document.querySelectorAll('.header__li');
+const sectionAll = document.querySelectorAll('section');
 const homeContainer = document.querySelector('#homeContainer');
 const categories = document.querySelector('#categoryContainer');
 
 let opacity;
 
-// Global Func
-const activateList = (li) => {
-  lists.forEach((list) => {
-    list.classList.remove('active');
-  });
-  li.classList.add('active');
+// Activate navigation button when scrolling
+const sections = Array.from(sectionAll);
+const listArray = Array.from(listAll);
+const sectionId = sections.map((i) => i.id);
+
+let selectedListIndex = 0;
+let selectedList = listArray[0];
+
+const activateBtn = (selected) => {
+  selectedList.classList.remove('active');
+  selectedList = selected;
+  selectedList.classList.add('active');
 };
+
+const options = {
+  root: null, //viewport
+  rootMargin: '0px',
+  threshold: 0.45,
+};
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionId.indexOf(entry.target.id);
+
+      if (entry.boundingClientRect.y < 0) {
+        selectedListIndex = index + 1;
+      } else {
+        selectedListIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+sections.forEach((section) => observer.observe(section));
 
 // Handle menubar button on mobile screen
 const handleMenu = () => {
@@ -28,27 +57,6 @@ const handleMenu = () => {
 // Scroll Events on header, home, and arrow up button
 const handleScroll = () => {
   const scrollY = window.scrollY;
-
-  // Active navbar with scroll event
-  sections.forEach((section) => {
-    const top = section.offsetTop;
-    const height = section.offsetHeight;
-    const id = section.getAttribute('id');
-
-    // condition : top and bottom of each section
-    if (scrollY >= top - 100 && scrollY < height + top) {
-      const target = document.querySelector(`[href='#${id}']`).parentElement;
-      activateList(target);
-    }
-  });
-
-  // condition for the active contact button
-  if (window.innerHeight + scrollY >= document.body.scrollHeight) {
-    const contactList = document.querySelector(
-      '.header__li [href="#contact"]'
-    ).parentElement;
-    activateList(contactList);
-  }
 
   // Header Scroll Event
   const headerHeight = header.getBoundingClientRect().height;
@@ -140,11 +148,15 @@ const handleWorks = (event) => {
 };
 
 // EventListeners
+window.addEventListener('scroll', () => {
+  if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.scrollHeight
+  ) {
+    selectedListIndex = listArray.length - 1;
+  }
+  activateBtn(listArray[selectedListIndex]);
+});
 document.addEventListener('scroll', handleScroll);
 menubarBtn.addEventListener('click', handleMenu);
-lists.forEach((list) => {
-  list.addEventListener('click', function () {
-    activateList(this);
-  });
-});
 categories.addEventListener('click', handleWorks);
